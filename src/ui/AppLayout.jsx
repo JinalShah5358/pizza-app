ed78ac24e4f3755b9ca5c6b42aae5a367e10d00c
171201 <jinalshah5358@gmail.com>
2024-12-18 23:14:16 +0530

    add loader logic

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,21 @@
+import { Outlet, useNavigation } from "react-router-dom";
+import CartOverview from "../features/cart/CartOverview";
+import Header from "./Header";
+import Loader from "./Loader";
+
+function AppLayout() {
+  const navigation = useNavigation();
+  console.log(navigation);
+  const isLoading = navigation.state === "loading";
+  return (
+    <div className="layout">
+      {isLoading && <Loader />}
+      <Header />
+      <main>content</main>
+      <Outlet />
+      <CartOverview />
+    </div>
+  );
+}
+
+export default AppLayout;
---
+++ src/ui/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Pizza App</Link>
+    </header>
+  );
+}
+
+export default Header;
