0099a81a79f1d9842cf5b294b67ddbed708e0e46
171201 <jinalshah5358@gmail.com>
2024-12-21 13:51:36 +0530

    add error handle logic

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -2,11 +2,10 @@ import { Outlet, useNavigation } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
 import Loader from "./Loader";
-import SearchOrder from "../features/order/SearchOrder";
 
 function AppLayout() {
   const navigation = useNavigation();
-  console.log(navigation);
+
   const isLoading = navigation.state === "loading";
   return (
     <div className="layout">
